9b8f3a72bee0788cb3fe2906d7f64e55766a2e28
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-05-15 17:42:15 +0530

    create frontend-project files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <>
+     <button className="btn btn-primary">click</button>
+    </>
+  )
+}
+
+export default App
