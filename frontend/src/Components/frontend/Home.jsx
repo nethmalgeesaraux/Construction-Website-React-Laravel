eea7399aa0890edaaf1f65f4569dace742949705
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-05-16 23:38:17 +0530

    enhance Home component with hero section and footer styling

--- frontend/src/Components/frontend/Home.jsx
+++ frontend/src/Components/frontend/Home.jsx
@@ -3,24 +3,47 @@ import Nav from 'react-bootstrap/Nav';
 
 const Home = () => {
     return (
-        <header>
-            <div className='container py-3'>
-                <Navbar expand='lg'>
-                    <Navbar.Brand href='#home'className='logo'><span>Build</span> Ease</Navbar.Brand>
-                    <Navbar.Toggle aria-controls='basic-navbar-nav' />
-                    <Navbar.Collapse id='basic-navbar-nav'>
-                        <Nav className='ms-auto'>
-                            <Nav.Link href='#home' className='nav-link'>Home</Nav.Link>
-                            <Nav.Link href='/about' className='nav-link'>About Us</Nav.Link>
-                            <Nav.Link href='#link' className='nav-link'>Services</Nav.Link>
-                            <Nav.Link href='#link' className='nav-link'>Projects</Nav.Link>
-                            <Nav.Link href='#link' className='nav-link'>Blogs</Nav.Link>
-                            <Nav.Link href='#link' className='nav-link'>Contact Us</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
-            </div>
-        </header>
+        <>
+            <header>
+                <div className='container py-3'>
+                    <Navbar expand='lg'>
+                        <Navbar.Brand href='#home' className='logo'><span>Build</span> Ease</Navbar.Brand>
+                        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                        <Navbar.Collapse id='basic-navbar-nav'>
+                            <Nav className='ms-auto'>
+                                <Nav.Link href='#home' className='nav-link'>Home</Nav.Link>
+                                <Nav.Link href='/about' className='nav-link'>About Us</Nav.Link>
+                                <Nav.Link href='#link' className='nav-link'>Services</Nav.Link>
+                                <Nav.Link href='#link' className='nav-link'>Projects</Nav.Link>
+                                <Nav.Link href='#link' className='nav-link'>Blogs</Nav.Link>
+                                <Nav.Link href='#link' className='nav-link'>Contact Us</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+                </div>
+            </header>
+
+
+            <main>
+                <section className='section-1'>
+                    <div className='hero d-flex align-items-center'>
+                        <div className='container-fluid text-center'>
+                            <div className='text-center'>
+                                <span>Welcome Amazing Constructions</span>
+                                <h1>Crafting dreams with <br />precision and excellence.</h1>
+                                <p>We excel at transforming visions into reality through outstanding craftsmanship and precise</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
+
+
+            <footer>
+
+            </footer>
+        </>
+
 
     )
 }
