ef6e12015d8c39133d112ebef9f3956c492a1af0
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-05-17 17:21:55 +0530

    update Home component to include them and modify styles for buttons and footer

--- frontend/src/Components/frontend/Home.jsx
+++ frontend/src/Components/frontend/Home.jsx
@@ -1,29 +1,12 @@
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
 import AboutImg from '../../assets/about-us.jpg';
+import Footer from '../pages/Footer';
+import Hearder from '../pages/Header';
 
 const Home = () => {
     return (
         <>
-            <header>
-                <div className='container py-3'>
-                    <Navbar expand='lg'>
-                        <Navbar.Brand href='#home' className='logo'><span>Build</span> Ease</Navbar.Brand>
-                        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-                        <Navbar.Collapse id='basic-navbar-nav'>
-                            <Nav className='ms-auto'>
-                                <Nav.Link href='#home' className='nav-link'>Home</Nav.Link>
-                                <Nav.Link href='/about' className='nav-link'>About Us</Nav.Link>
-                                <Nav.Link href='#link' className='nav-link'>Services</Nav.Link>
-                                <Nav.Link href='#link' className='nav-link'>Projects</Nav.Link>
-                                <Nav.Link href='#link' className='nav-link'>Blogs</Nav.Link>
-                                <Nav.Link href='#link' className='nav-link'>Contact Us</Nav.Link>
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Navbar>
-                </div>
-            </header>
-
+            
+            <Hearder />
 
             <main>
 
@@ -66,71 +49,10 @@ const Home = () => {
 
             </main>
 
+            <Footer />
 
-            <footer>
-                <div className="container footer-wrap">
-                    <div className="row footer-top">
-                        {/* Column 1 - Company Info */}
-                        <div className="col-12 col-sm-6 col-lg-3 footer-col">
-                            <h3 className="text-orange mb-3">UrbanEdge Constructions</h3>
-                            <div className="pe-4">
-                                <p>
-                                    Our post-construction services gives you peace of mind knowing that
-                                    we are still here for you even after.
-                                </p>
-                            </div>
-                        </div>
 
-                        {/* Column 2 - Services */}
-                        <div className="col-12 col-sm-6 col-lg-3 footer-col">
-                            <h3 className="mb-3 text-orange">Our Service</h3>
-                            <ul className="footer-list">
-                                <li><a href="">Specialty Construction</a></li>
-                                <li><a href="">Civil Construction</a></li>
-                                <li><a href="">Residential Construction</a></li>
-                                <li><a href="">Corporate Construction</a></li>
-                                <li><a href="">Building Constructions</a></li>
-                                <li><a href="">Industrial Construction</a></li>
-                            </ul>
-                        </div>
-
-                        {/* Column 3 - Quick Links */}
-                        <div className="col-12 col-sm-6 col-lg-3 footer-col">
-                            <h3 className="mb-3 text-orange">Quick Links</h3>
-                            <ul className="footer-list">
-                                <li><a href="">About Us</a></li>
-                                <li><a href="">Services</a></li>
-                                <li><a href="">Projects</a></li>
-                                <li><a href="">Blog</a></li>
-                                <li><a href="">Contact Us</a></li>
-                            </ul>
-                        </div>
-
-                        {/* Column 4 - Contact */}
-                        <div className="col-12 col-sm-6 col-lg-3 footer-col">
-                            <h3 className="mb-3 text-orange">Contact Us</h3>
-                            <ul className="footer-list">
-                                <li><a href="tel:8880000000">(888-000-0000)</a></li>
-                                <li><a href="mailto:info@example.com">info@example.com</a></li>
-                                <li>
-                                    <a href="">
-                                        (Sgaramawatha) Pandura, Colombo<br />
-                                        0522400XXXX
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    {/* Copyright */}
-                    <div className="text-center footer-bottom">
-                        <small>
-                            Copyright &copy; 2026 Build Ease
-                            Constructions. All Rights Reserved.
-                        </small>
-                    </div>
-                </div>
-            </footer>
+            
 
         </>
 
---
+++ frontend/src/Components/pages/Header.jsx
@@ -0,0 +1,28 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+const Header = () => {
+  return (
+    <div>
+        <header>
+                <div className='container py-3'>
+                    <Navbar expand='lg'>
+                        <Navbar.Brand href='#home' className='logo'><span>Build</span> Ease</Navbar.Brand>
+                        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                        <Navbar.Collapse id='basic-navbar-nav'>
+                            <Nav className='ms-auto'>
+                                <Nav.Link href='#home' className='nav-link'>Home</Nav.Link>
+                                <Nav.Link href='/about' className='nav-link'>About Us</Nav.Link>
+                                <Nav.Link href='#link' className='nav-link'>Services</Nav.Link>
+                                <Nav.Link href='#link' className='nav-link'>Projects</Nav.Link>
+                                <Nav.Link href='#link' className='nav-link'>Blogs</Nav.Link>
+                                <Nav.Link href='#link' className='nav-link'>Contact Us</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+                </div>
+            </header>
+    </div>
+  )
+}
+
+export default Header
