1409de278f8db1469302a093159422d8fd25af76
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-05-21 21:37:38 +0530

    add ContactUs component with header and footer

---
+++ frontend/src/Components/frontend/ContactUs.jsx
@@ -0,0 +1,25 @@
+import Footer from "../pages/Footer"
+import Header from "../pages/Header"
+const ContactUs = () => {
+    return (
+        <div>
+            <Header />
+
+            <section className='section-7'>
+                <div className='hero d-flex align-items-center'>
+                    <div className='container'>
+                        <div className='text-left'>
+                            <span>Quality. Integrity. Value.</span>
+                            <h1>ContactUs</h1>
+                            <p>We excel at transforming visions into reality <br />through outstanding craftsmanship and precise.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <Footer />
+        </div>
+    )
+}
+
+export default ContactUs
