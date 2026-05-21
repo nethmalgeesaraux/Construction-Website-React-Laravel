3aec34f6398671b951012482e5909ccf49894c77
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-05-21 21:35:14 +0530

    add Blogs component with header and hero section

---
+++ frontend/src/Components/frontend/Blogs.jsx
@@ -0,0 +1,26 @@
+import Footer from "../pages/Footer"
+import Header from "../pages/Header"
+
+const Blogs = () => {
+    return (
+        <div>
+            <Header />
+
+            <section className='section-7'>
+                <div className='hero d-flex align-items-center'>
+                    <div className='container'>
+                        <div className='text-left'>
+                            <span>Quality. Integrity. Value.</span>
+                            <h1>Blogs</h1>
+                            <p>We excel at transforming visions into reality <br />through outstanding craftsmanship and precise.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <Footer />
+        </div>
+    )
+}
+
+export default Blogs
