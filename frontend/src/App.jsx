d2ee737069ebc84b808ed4a02d54b1a264971ac4
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-05-21 21:40:05 +0530

    add routing for navigation links

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Components/frontend/Home';
 import About from './Components/frontend/About';
+import Services from './Components/frontend/Services';
+import Projects from './Components/frontend/Projects';
+import Blogs from './Components/frontend/Blogs';
+import ContactUs from './Components/frontend/ContactUs';
 import './assets/css/style.scss';
 
 function App() {
@@ -11,7 +15,12 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-           <Route path="/about" element={<About />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/Services" element={<Services />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/contact" element={<ContactUs />} />
         </Routes>
       </BrowserRouter>
     </>
--- frontend/src/Components/pages/Header.jsx
+++ frontend/src/Components/pages/Header.jsx
@@ -1,21 +1,23 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import { Link, NavLink } from 'react-router-dom';
+
 const Header = () => {
   return (
     <div>
         <header>
                 <div className='container py-3'>
                     <Navbar expand='lg'>
-                        <Navbar.Brand href='/' className='logo'><span>Build</span> Ease</Navbar.Brand>
+                        <Navbar.Brand as={Link} to='/' className='logo'><span>Build</span> Ease</Navbar.Brand>
                         <Navbar.Toggle aria-controls='basic-navbar-nav' />
                         <Navbar.Collapse id='basic-navbar-nav'>
                             <Nav className='ms-auto'>
-                                <Nav.Link href='/' className='nav-link'>Home</Nav.Link>
-                                <Nav.Link href='/about' className='nav-link'>About Us</Nav.Link>
-                                <Nav.Link href='/service' className='nav-link'>Services</Nav.Link>
-                                <Nav.Link href='project' className='nav-link'>Projects</Nav.Link>
-                                <Nav.Link href='#link' className='nav-link'>Blogs</Nav.Link>
-                                <Nav.Link href='#link' className='nav-link'>Contact Us</Nav.Link>
+                                <Nav.Link as={NavLink} to='/' className='nav-link'>Home</Nav.Link>
+                                <Nav.Link as={NavLink} to='/about' className='nav-link'>About Us</Nav.Link>
+                                <Nav.Link as={NavLink} to='/services' className='nav-link'>Services</Nav.Link>
+                                <Nav.Link as={NavLink} to='/projects' className='nav-link'>Projects</Nav.Link>
+                                <Nav.Link as={NavLink} to='/blogs' className='nav-link'>Blogs</Nav.Link>
+                                <Nav.Link as={NavLink} to='/contact' className='nav-link'>Contact Us</Nav.Link>
                             </Nav>
                         </Navbar.Collapse>
                     </Navbar>
