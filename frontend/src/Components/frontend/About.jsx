5e8becc6dc2421ca781afbfe9bdbcf911c064ca6
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-05-21 15:57:40 +0530

    add About section with hero

--- frontend/src/Components/frontend/About.jsx
+++ frontend/src/Components/frontend/About.jsx
@@ -5,7 +5,20 @@ const About = () => {
   return (
     <div>
       <Hearder />
-      <main> About</main>
+      <main>
+        <section className='section-7'>
+          <div className='hero d-flex align-items-center'>
+            <div className='container'>
+              <div className='text-left'>
+                <span>Quality. Integrity. Value.</span>
+                <h1>About Us</h1>
+                <p>We excel at transforming visions into reality <br />through outstanding craftsmanship and precise.</p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+
       <Footer />
     </div>
   )
