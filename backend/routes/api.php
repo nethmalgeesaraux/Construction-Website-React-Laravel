210e886033b96807166ee7b8cc6b9669c454f88c
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-05-24 22:07:48 +0530

    test AuthenticationController and define route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthenticationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::post('authenticate',[AuthenticationController::class,'authenticate']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
