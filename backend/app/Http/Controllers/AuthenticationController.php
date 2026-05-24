54a54bd41b49c9380b051ddcb6c4b8a55734c336
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-05-24 22:23:00 +0530

    Implement authentication logic with validation in AuthenticationController

--- backend/app/Http/Controllers/AuthenticationController.php
+++ backend/app/Http/Controllers/AuthenticationController.php
@@ -2,11 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
+
 
 class AuthenticationController extends Controller
 {
-    public function authenticate(){
-        echo "Hello World";
+    public function authenticate(Request $request)
+    {
+
+        // Apply Validation
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors()
+            ]);
+        } else {
+            $credentials = [
+                'email' => $request->email,
+                'password' => $request->password,
+            ];
+
+            if (Auth::attempt($credentials)) {
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Email/password is incorrect.'
+                ]);
+            }
+        }
     }
 }
