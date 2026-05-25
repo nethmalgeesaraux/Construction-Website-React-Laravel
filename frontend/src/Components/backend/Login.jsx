1e7799a154d8f010d33b771391359b9a9c72573a
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-05-25 23:22:18 +0530

    Add react-hook-form for form handling in Login component

--- frontend/src/Components/backend/Login.jsx
+++ frontend/src/Components/backend/Login.jsx
@@ -1,7 +1,16 @@
 import Footer from "../common/Footer"
 import Header from "../common/Header"
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const showPassword = watch("showPassword", false);
+    const onSubmit = data => {
+        console.log(data);
+    }
+
+
     return (
         <>
             <Header />
@@ -10,19 +19,55 @@ const Login = () => {
                     <div className='login-form my-5 w-50 mx-auto'>
                         <div className='card border-0 shadow'>
                             <div className='card-body py-5'>
-                                <form>
+                                <form onSubmit={handleSubmit(onSubmit)}>
                                     <h4 className='mb-4 text-center'>Login</h4>
                                     <div className='mb-3'>
                                         <label htmlFor='' className='form-label'>Email</label>
-                                        <input type='text' placeholder='Email' className='form-control' />
+                                        <input
+                                            {...register('email', {
+                                                required: "This field is required",
+                                                pattern: {
+                                                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                                    message: "invalid email address"
+                                                }
+
+                                            })}
+                                            type="text" placeholder='Email'
+                                            className={`form-control ${errors.email && 'is-invalid'}`}
+                                        />
+                                        {errors.email && <p className='invalid-feedback'>{errors.email?.message}</p>}
+
                                     </div>
 
-                                     <div className='mb-3'>
+                                    <div className='mb-3'>
                                         <label htmlFor='' className='form-label'>Password</label>
-                                        <input type='password' placeholder='Password' className='form-control' />
+                                        <input
+                                            {...register('password', {
+                                                required: "This field is required",
+                                                pattern: {
+                                                    value: /^.{8,}$/,
+                                                    message: "Password must be at least 8 characters"
+                                                }
+
+
+                                            })}
+                                            type={showPassword ? "text" : "password"} placeholder='Password'
+                                            className={`form-control ${errors.password && 'is-invalid'}`}
+                                        />
+                                        <div className="form-check mt-2">
+                                            <input
+                                                id="showPassword"
+                                                type="checkbox"
+                                                className="form-check-input"
+                                                {...register("showPassword")}
+                                            />
+                                            <label htmlFor="showPassword" className="form-check-label">Show password</label>
+                                        </div>
+                                        {errors.password && <p className='invalid-feedback'>{errors.password?.message}</p>}
+
                                     </div>
 
-                                    <button className='btn btn-primary'>Login</button>
+                                    <button type="submit" className='btn btn-primary'>Login</button>
 
                                 </form>
                             </div>
