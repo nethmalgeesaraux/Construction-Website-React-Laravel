91704bedfc29a741f8908b3b3cbccbe60dc9f88e
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-05-27 22:23:43 +0530

    Enhance sidebar styles and add new components

--- frontend/src/Components/common/Sidebar.jsx
+++ frontend/src/Components/common/Sidebar.jsx
@@ -1,18 +1,25 @@
-
+import { useContext } from 'react'
+import { AuthContext } from '../backend/context/Auth';
 
 const Sidebar = () => {
+
+    const { logout } = useContext(AuthContext)
+
     return (
-        <div className='card shadow border-0'>
-            <div className='card-body'>
-                <h4>sidebar</h4>
-                <ul>
-                    <li><a href="#">Dashboard</a></li>
-                    <li><a href="#">Services</a></li>
-                    <li><a href="#">Projects</a></li>
-                    <li><a href="#">Articles</a></li>
-                    <li><button class="btn btn-primary">Logout</button></li>
+        <div className='card shadow border-0 admin-sidebar-card'>
+            <div className='card-body sidebar'>
+                <h4 className='sidebar-title'>Sidebar</h4>
+
+                <ul className='sidebar-nav'>
+                    <li><a href="#" className='sidebar-link'>Dashboard</a></li>
+                    <li><a href="#" className='sidebar-link'>Services</a></li>
+                    <li><a href="#" className='sidebar-link'>Projects</a></li>
+                    <li><a href="#" className='sidebar-link'>Articles</a></li>
                 </ul>
 
+                <button onClick={logout} className='btn sidebar-logout-btn mt-3'>
+                    Logout
+                </button>
             </div>
         </div>
     )
