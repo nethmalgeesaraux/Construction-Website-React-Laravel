5924f8d97d4d7377c3259c66d063ef5f40b2e52f
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-05-28 00:26:26 +0530

    Add image processing functionality to TempImageController and update dependencies

--- backend/app/Http/Controllers/admin/TempImageController.php
+++ backend/app/Http/Controllers/admin/TempImageController.php
@@ -6,6 +6,10 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\TempImage;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver as GdDriver;
+use Intervention\Image\Drivers\Imagick\Driver as ImagickDriver;
+
 
 class TempImageController extends Controller
 {
@@ -24,21 +28,42 @@ class TempImageController extends Controller
 
         $image = $request->image;
 
-         $ext = $image->getClientOriginalExtension();
-            $imageName = strtotime('now') . '.' . $ext;
+        $ext = $image->getClientOriginalExtension();
+        $imageName = strtotime('now') . '.' . $ext;
 
-            // Save image info to database
-            $model = new TempImage();
-            $model->name = $imageName;
-            $model->save();
+        // Save image info to database
+        $model = new TempImage();
+        $model->name = $imageName;
+        $model->save();
 
-            // Move the uploaded image to the desired location
-            $image->move(public_path('uploads/temp'), $imageName);
+        // Move the uploaded image to the desired location
+        $image->move(public_path('uploads/temp'), $imageName);
 
-            return response()->json([
-                'status' => true,
-                'data' => $model,
-                'message' => 'Image uploaded successfully.'
-            ]);
+        // Create small thumbnail here
+        $sourcePath = public_path('uploads/temp/' . $imageName);
+        $destPath = public_path('uploads/temp/thumb/' . $imageName);
+
+        if (!is_dir(public_path('uploads/temp/thumb'))) {
+            mkdir(public_path('uploads/temp/thumb'), 0755, true);
+        }
+
+        if (extension_loaded('gd')) {
+            $manager = new ImageManager(GdDriver::class);
+            $image = $manager->decodePath($sourcePath);
+            $image->coverDown(300, 300);
+            $image->save($destPath);
+        } elseif (extension_loaded('imagick')) {
+            $manager = new ImageManager(ImagickDriver::class);
+            $image = $manager->decodePath($sourcePath);
+            $image->coverDown(300, 300);
+            $image->save($destPath);
+        }
+
+
+        return response()->json([
+            'status' => true,
+            'data' => $model,
+            'message' => 'Image uploaded successfully.'
+        ]);
     }
 }
